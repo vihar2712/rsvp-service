563e02d67bba9fc08c39d0eb46f0588f2b726c6e
vihar2712 <viharshah222@gmail.com>
2025-04-16 13:36:22 -0400

    added title

--- src/components/RsvpService.tsx
+++ src/components/RsvpService.tsx
@@ -7,16 +7,19 @@ export function RsvpService(){
     const allPlayers:Player[] = JSON.parse(localStorage.getItem("players")!) || [];
 
     return (
-        <div className="w-1/4 mx-auto">
-            <List list={allPlayers} showBtn={true} title="List of All Players"/>
-            {/* <h1> Update Status </h1>
-            <h1> List of all confirmed attendees</h1>
-            <h1> Total responses</h1>
-            <h1> Confirmed responses</h1>
-            <h1> Declined responses</h1> */}
-            <div className="mt-2 flex gap-3">
-                <button className="" onClick={() => navigate('/add-player') }>Add Player</button>
-                <button className="" onClick={() => navigate('/stats') }>Stats</button>
+        <div>
+            <h1 className="text-center text-gray-400 font-semibold"> Welcome to GametimeHero RSVP Feature</h1>
+            <div className="w-1/4 mx-auto mt-5">
+                <List list={allPlayers} showBtn={true} title="List of All Players"/>
+                {/* <h1> Update Status </h1>
+                <h1> List of all confirmed attendees</h1>
+                <h1> Total responses</h1>
+                <h1> Confirmed responses</h1>
+                <h1> Declined responses</h1> */}
+                <div className="mt-2 flex gap-3">
+                    <button className="" onClick={() => navigate('/add-player') }>Add Player</button>
+                    <button className="" onClick={() => navigate('/stats') }>Stats</button>
+                </div>
             </div>
         </div>
     )
